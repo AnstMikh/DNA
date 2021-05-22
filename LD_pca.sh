18da2941b9efeba55459fe5aa06d5b3541b6f3fc
AnstMikh <62886473+AnstMikh@users.noreply.github.com>
2021-05-22 10:19:48 +0300

    Update LD_pca.sh

--- LD_pca.sh
+++ LD_pca.sh
@@ -10,5 +10,5 @@ source activate p2
 cd ~/plink/wight
 srun plink --vcf ~/data.vcf.gz --double-id --vcf-half-call m --chr 1-22 --make-bed --out data
 srun plink --bfile data --indep-pairwise 50 10 .5 --chr 1-22 --geno 0.98 --out ~/plink/light_data
-srun plink --bfile data --allow-extra-chr --extract ~/plink/light_data.prune.in --make-bed --out light_data
+srun plink --bfile data --extract ~/plink/light_data.prune.in --make-bed --out light_data
 srun plink --bfile data --pca --out data
