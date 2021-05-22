ba57b9dcf8b248071857ff5c6556f7dc385581de
AnstMikh <62886473+AnstMikh@users.noreply.github.com>
2021-05-22 10:27:58 +0300

    Update LD_pca.sh

--- LD_pca.sh
+++ LD_pca.sh
@@ -9,6 +9,6 @@ module load Python/Anaconda_v10.2019
 source activate p2
 cd ~/plink/wight
 srun plink --vcf ~/data.vcf.gz --double-id --vcf-half-call m --chr 1-22 --make-bed --out data
-srun plink --bfile data --indep-pairwise 50 10 .5 --chr 1-22 --geno 0.98 --out ~/plink/light_data
+srun plink --bfile data --indep-pairwise 50 10 .5 --chr 1-22 --geno 0.98 --snps-only  --maf 0.05 --out ~/plink/light_data
 srun plink --bfile data --extract ~/plink/light_data.prune.in --make-bed --out light_data
 srun plink --bfile data --pca --out data
